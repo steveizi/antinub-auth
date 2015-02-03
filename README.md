6a57a6ffdb5ba59a7dee83714bd706925983ed8a
randomic <j.a.d.mcmillan@gmail.com>
2015-02-03 23:40:35 +0000

    Made working extension of default User model.

---
+++ README.md
@@ -0,0 +1,2 @@
+# antinub-auth
+Auth site, written using Django, for Services relating to the operation of an EVE Online corporation.
---
+++ antinubinspace/aniauth/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+
+
+class ANIUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password,
+                     is_staff, is_superuser, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email,
+                          is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False,
+                                 **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True,
+                                 **extra_fields)
+
+
+class ANIUser(AbstractBaseUser, PermissionsMixin):
+    """
+    An class implementing a fully featured ANIUser model with
+    admin-compliant permissions.
+    Email and password are required. Other fields are optional.
+    """
+    email = models.EmailField(_('email address'), unique=True,
+        error_messages={
+            'unique': _("A user with that email already exists."),
+        })
+    is_staff = models.BooleanField(_('staff status'), default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+        help_text=_('Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = ANIUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        """
+        ANIUsers are identified by email address.
+        """
+        return self.email
+
+    def get_short_name(self):
+        """
+        ANIUsers are identified by email address.
+        """
+        return self.email
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email], **kwargs)
