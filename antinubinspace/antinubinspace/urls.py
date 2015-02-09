0ae83144e3265c5829a82492e1e893735cede985
randomic <j.a.d.mcmillan@gmail.com>
2015-02-09 21:54:02 +0000

    Messing around with base.html

--- antinubinspace/antinubinspace/urls.py
+++ antinubinspace/antinubinspace/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'antinubinspace.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
+    url(r'^$', 'aniauth.views.index', name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
